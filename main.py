a140fbc5637a00a835891740b492f56603507d53
kmnseo <ms060805@naver.com>
2025-12-09 14:07:35 +0900

    main.py
    Add a simple calculator for sum and product of two numbers.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-text file a
+# main.py
+
+# 두 수 입력받기
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+
+# 정수로 변환
+num1 = int(first)
+num2 = int(second)
+
+#합, 차, 곱 계산
+sum_result =num1 + num2
+mul_result = num1 * num2
+
+#결과 출력
+print("Sum:", sum_result)
+print("Product:", mul_result)
